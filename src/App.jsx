44d0451b1e98dcca3d69b909e5845be38f36b30c
AKSUF <abu053125@gmail.com>
2023-05-08 18:53:05 +0600

    get data without api

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import './App.css';
 import ComplexCounter from './components/Complexounter3';
 import Counter from './components/Counter';
 import Main from './components/Main';
+import Main2 from './components/Main2';
 
 
 function App() {
   return (
     <div className="App">
 {/* <Counter></Counter> */}
-<Main></Main>
+<Main2></Main2>
     </div>
   );
 }
---
+++ src/components/Main2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import GetPost from './GetPost'
+
+const Main2 = () => {
+    return (
+        <div>
+     <GetPost></GetPost>
+        </div>
+    )
+}
+
+export default Main2
