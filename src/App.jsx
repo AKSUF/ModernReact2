22eb40801e7c572a30f71a2d5aad8b3795201410
AKSUF <abu053125@gmail.com>
2023-05-08 20:50:36 +0600

    how to use custoom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 
 import './App.css';
-import ComplexCounter from './components/Complexounter3';
-import Counter from './components/Counter';
-import Main from './components/Main';
-import Main2 from './components/Main2';
-
+import LayoutComponentOne from './components/LayoutComponentOne';
 
 function App() {
   return (
     <div className="App">
-{/* <Counter></Counter> */}
-<Main2></Main2>
+<LayoutComponentOne></LayoutComponentOne>
     </div>
   );
 }
