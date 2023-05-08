70c7ca116212664f69591136396e163aef9f4f17
AKSUF <abu053125@gmail.com>
2023-05-08 20:57:50 +0600

    use scall in className

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.css';
 import LayoutComponentOne from './components/LayoutComponentOne';
+import LayoutComponentTwo from './components/LayoutComponentTwo';
 
 function App() {
   return (
     <div className="App">
-<LayoutComponentOne></LayoutComponentOne>
+<LayoutComponentTwo></LayoutComponentTwo>
     </div>
   );
 }
